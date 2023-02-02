6b60acdf0b5d088975cd0ff83ed496b29f53db14
Patrick Schönfeld <patrick.schoenfeld@cec.valantic.com>
2023-02-02 11:23:36 +0100

    no-task add elasticsearch-logging related classes

---
+++ src/ValanticSpryker/Yves/Log/LogConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace ValanticSpryker\Yves\Log;
+
+use ValanticSpryker\Shared\Log\LogConstants;
+use Spryker\Glue\Log\LogConfig as SprykerLogConfig;
+
+class LogConfig extends SprykerLogConfig
+{
+    /**
+     * @return int
+     */
+    public function getMaxNormalizeDepth(): int
+    {
+        return $this->get(LogConstants::MAX_NORMALIZE_DEPTH, 9);
+    }
+}
