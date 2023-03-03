5cfd4591b9c83ac8b31ce27e18f5c2504efbdf72
Patrick Schönfeld <patrick.schoenfeld@cec.valantic.com>
2023-03-03 11:09:15 +0100

    no-task add tests

--- tests/ValanticSprykerTest/Shared/Log/Processor/RequestProcessorTest.php
+++ tests/ValanticSprykerTest/Shared/Log/Processor/RequestProcessorTest.php
@@ -5,13 +5,8 @@ declare(strict_types = 1);
 namespace ValanticSprykerTest\Shared\Log\Processor;
 
 use Codeception\Test\Unit;
-use Spryker\Shared\Config\Config;
 use Spryker\Shared\Log\Sanitizer\SanitizerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use ValanticSpryker\Shared\Log\Processor\RequestProcessor;
-use ValanticSpryker\Shared\SearchElasticsearch\ElasticaClient\ElasticaClientFactory;
-use ValanticSpryker\Zed\SearchElasticsearch\Business\SearchElasticsearchBusinessFactory;
 
 /**
  * Auto-generated group annotations
